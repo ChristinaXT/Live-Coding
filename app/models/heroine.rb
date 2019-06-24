6b8632d149a23f50b96b9ad285dd2d215c9803c7
Guess <guess@Guesss-iMac.local>
2019-06-24 10:03:53 -0400

    practiced search bar

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -3,4 +3,17 @@ class Heroine < ApplicationRecord
 
   validates :super_name, uniqueness: true
   validates :name, presence: true
+
+  def self.search(search)
+    if search
+      power = Power.find_by(name: search)
+      if power
+        self.where(power_id: power)
+      else
+        Herione.all
+      end
+    else
+      Heroine.all
+    end
+  end
 end
